7bcc8a28bdd14a56ba287967e9af124414696561
thecoolguy2001 <demontewalker12@gmail.com>
2025-05-10 22:59:11 -0400

    spotify

--- api/spotify-login.js
+++ api/spotify-login.js
@@ -1,15 +1,22 @@
 // /api/spotify-login.js
 export default function handler(req, res) {
-    const params = new URLSearchParams({
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      response_type: 'code',
-      redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-      scope: [
-        'streaming',
-        'user-read-playback-state',
-        'user-modify-playback-state',
-        'user-top-read'
-      ].join(' ')
-    });
-    res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
-  }
+  // Build the redirect URI dynamically based on request origin
+  const origin = req.headers.origin;
+  const redirectUri = process.env.SPOTIFY_REDIRECT_URI || `${origin}/api/spotify-callback`;
+
+  // Set up Spotify authorization parameters
+  const params = new URLSearchParams({
+    client_id: process.env.SPOTIFY_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: redirectUri,
+    scope: [
+      'streaming',
+      'user-read-playback-state',
+      'user-modify-playback-state',
+      'user-top-read'
+    ].join(' ')
+  });
+
+  // Redirect the user to Spotify's authorization endpoint
+  res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
+}
